cedb66e8d02d6ebc4a2f17356b3cedb98f52e2ba
Charles Lambert <charles@mbp-de-charles.lan>
2022-07-26 14:34:01 +0200

    cart total

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import { useState, useEffect } from "react";
 import "./App.scss";
 import axios from "axios";
 import Header from "./components/Header";
-import Card from "./components/Card";
+import bgDeliveroo from "./assets/img/bg-deliveroo.svg";
+// import Card from "./components/Card";
 
 function App() {
   const [data, setData] = useState();
   const [isLoading, setIsLoading] = useState(true);
+  const [title, setTitle] = useState([]);
+  const [counter, setCounter] = useState([]);
+  const [total, setTotal] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -25,6 +29,24 @@ function App() {
     fetchData();
   }, []);
 
+  // const newTotal = counter + 2.5;
+
+  const handleClickCartAdd = (index) => {
+    const newTitle = [...title];
+    newTitle[index] = newTitle + data.categories[0].meals[0].title;
+    setTitle(newTitle);
+
+    const newCounter = [...counter];
+    newCounter[index] = newCounter + data.categories[0].meals[0].price;
+    setCounter(newCounter);
+
+    const newTotal = [...total];
+    let totalMeal = data.categories[0].meals[0].price;
+    let fraisDeLivraison = 2.5;
+    newTotal[index] = newTotal + totalMeal + fraisDeLivraison;
+    setTotal(newTotal);
+  };
+
   return (
     <div className="App">
       {isLoading === true ? (
@@ -38,24 +60,61 @@ function App() {
               <div className="menu">
                 {data.categories.map((category, index) => {
                   return (
-                    <div key={index} className="container-food">
-                      <h2>{category.name}</h2>
-                      <div className="global-card">
-                        {category.meals.map((dish, index) => {
-                          return (
-                            <>
-                              <Card dish={dish} key={index} />
-                              {/* <Card dish={dish} /> */}
-                            </>
-                          );
-                        })}
+                    category.meals.length > 0 && (
+                      <div key={index} className="container-food">
+                        <h2>{category.name}</h2>
+                        <div className="global-card">
+                          {category.meals.map((dish, index) => {
+                            // return <Card dish={dish} key={index} />;
+                            return (
+                              <div key={index} className="card-dish">
+                                <div
+                                  onClick={() => {
+                                    handleClickCartAdd(index);
+                                  }}
+                                  className="dish"
+                                >
+                                  <div className="dish-text">
+                                    <h3>{dish.title}</h3>
+                                    <p className="dish-description">
+                                      {dish.description}
+                                    </p>
+                                    <p className="dish-price">{dish.price}</p>
+                                    <div className="tag">{dish.popular}</div>
+                                  </div>
+                                  <img
+                                    className="dish-picture"
+                                    src={
+                                      dish.picture ? dish.picture : bgDeliveroo
+                                    }
+                                    alt=""
+                                  />
+                                </div>
+                              </div>
+                            );
+                          })}
+                        </div>
                       </div>
-                    </div>
+                    )
                   );
                 })}
               </div>
-
-              <div className="shopping-cart"></div>
+              {/* //dklfslkfjgkldfjglkfdjgjdlfkj //dklfslkfjgkldfjglkfdjgjdlfkj
+              //dklfslkfjgkldfjglkfdjgjdlfkj //dklfslkfjgkldfjglkfdjgjdlfkj
+              //dklfslkfjgkldfjglkfdjgjdlfkj //dklfslkfjgkldfjglkfdjgjdlfkj */}
+              <div className="shopping-cart">
+                <div className="shopping-cart-card">
+                  <button>Valider mon panier</button>
+                  <div className="cart-validate">
+                    <p>{title}</p>
+                    <p>{counter}</p>
+                    <p>{total}</p>
+                  </div>
+                </div>
+              </div>
+              {/* //dklfslkfjgkldfjglkfdjgjdlfkj //dklfslkfjgkldfjglkfdjgjdlfkj
+              //dklfslkfjgkldfjglkfdjgjdlfkj //dklfslkfjgkldfjglkfdjgjdlfkj
+              //dklfslkfjgkldfjglkfdjgjdlfkj //dklfslkfjgkldfjglkfdjgjdlfkj */}
             </div>
           </div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
+import bgDeliveroo from "../assets/img/bg-deliveroo.svg";
+
 const Card = ({ dish }) => {
+  const [counter, setCounter] = useState([0]);
+
+  const handleClickCartAdd = () => {
+    const newCounter = [...counter];
+    newCounter = newCounter + 1;
+    setCounter(newCounter);
+  };
+
   return (
     <div className="card-dish">
-      <div className="dish">
+      <div onClick={handleClickCartAdd} className="dish">
         <div className="dish-text">
           <h3>{dish.title}</h3>
           <p className="dish-description">{dish.description}</p>
           <p className="dish-price">{dish.price}</p>
           <div className="tag">{dish.popular}</div>
         </div>
-        <img className="dish-picture" src={dish.picture} alt="Dish picture" />
+        <img
+          className="dish-picture"
+          src={dish.picture ? dish.picture : bgDeliveroo}
+          alt=""
+        />
       </div>
     </div>
   );
